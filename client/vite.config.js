199834cfe90fbf3793b71cdc979319629bc9be6b
Sonudhukia143 <jagdishdhukia770@gmail.com>
2025-03-25 09:38:56 +0530

    new state added

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://bank-website-23d3.vercel.app/',
+        target: 'http://localhost:3000/',
         changeOrigin: true,
         secure: false,
       }
